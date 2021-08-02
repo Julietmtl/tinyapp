340933c5391c3e23e8ccdbee863a1a9b1e136172
Juliet Li <li_juliet@hotmail.com>
2021-08-02 22:36:03 +0000

    added random code

--- express_server.js
+++ express_server.js
@@ -1,6 +1,14 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return  Math.random().toString(20).substr(2, 6)
+}
 
 app.set("view engine", "ejs");
 
@@ -16,6 +24,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -39,6 +51,11 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");        
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
